8208a13e0437cd45e05667f6b8b21418cdc98976
4f4d <148759936+4f4d@users.noreply.github.com>
2026-03-24 16:22:47 +0530

    minor change: removed token never called

--- src/components/BottomPanel.tsx
+++ src/components/BottomPanel.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 import Terminal from './Terminal';
 import OutputPanel from './OutputPanel';
 import ProblemsPanel from './ProblemsPanel';
